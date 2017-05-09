5bae5fe71ece3648f70d42cf5b73c42efda2d86d
Evgeny Zhernoklev <John@Mac-mini-Evgeny-2.local>
2017-05-09 12:20:24 +0300

    Scroll to elements on clicking menu

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var $window = $(window),
         $body = $('body');
 
-    /*************** check if browser supports svg ***************/
+    // check if browser supports svg
     var supportsSVG = !!document.createElementNS && !!document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect;
     if (!supportsSVG) {
         $body.addClass('no-svg');
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 
 
-    /*************** popup opening ***************/
+    // popup opening
     $body.on('click', '.popup-open', function(e) {
       e.preventDefault();
       var target = $(this).data('popup'),
@@ -25,5 +25,37 @@ $(document).ready(function() {
 
 
 
-    
+    // menu
+    var $menuWrapper = $('.menu'),
+        MENU_HEIGHT = $menuWrapper.height(),
+        $menuLinks = $menuWrapper.find('.menu-scroll'),
+        scrollTop = 0;
+
+    function scrollToElement(target) {
+      var targetPosition = $(target).offset().top - MENU_HEIGHT;
+
+      $('body, html').animate({
+        scrollTop: targetPosition
+      }, {
+        duration: 700
+      });
+    }
+
+    $window.on('load scroll', function() {
+      scrollTop = $window.scrollTop();
+
+      if (scrollTop > 0) {
+        $menuWrapper.addClass('is-scrolled');
+      } else {
+        $menuWrapper.removeClass('is-scrolled');
+      }
+    });
+
+    $menuLinks.on('click', function(e) {
+      e.preventDefault();
+      var target = $(this).attr('href');
+      scrollToElement(target);
+    });
+
+
 });
